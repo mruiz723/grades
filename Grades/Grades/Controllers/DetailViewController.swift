464fea3324dff637243f32c91266b473323df092
mruiz723 <centrodocentedecomputos@14-209-19.local>
2016-02-26 20:47:43 -0500

    Implement GradesViewController and DetailViewController.

---
+++ Grades/Grades/Controllers/DetailViewController.swift
@@ -0,0 +1,76 @@
+//
+//  DetailViewController.swift
+//  Grades
+//
+//  Created by S209e19 on 26/02/16.
+//  Copyright © 2016 UdeM. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    //MARK: IBOutlets
+    @IBOutlet weak var nameCourseLabel: UILabel!
+    @IBOutlet weak var gradeLabel: UILabel!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var gradeTextField: UITextField!
+    //MARK: IBActions
+    @IBAction func edit(sender: AnyObject) {
+        if editButton.selected{
+            editButton.selected = false
+            if let gradeFloat = Float(gradeTextField.text!){
+                if gradeFloat >= 0.0  && gradeFloat <= 5.0{
+                    gradeLabel.text = gradeTextField.text
+                    gradeLabel.hidden = false
+                    gradeTextField.hidden = true
+                }else{
+                    let alert = UIAlertController(title: "Error", message: "Debe ingresar un número entre 0.0 y 5.0", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                    
+                self.presentViewController(alert, animated: true, completion: nil)
+                }
+            }
+            
+        }else{
+            editButton.selected = true
+            gradeTextField.hidden = false
+            gradeLabel.hidden = true
+            gradeTextField.text = gradeLabel.text
+        }
+        
+    }
+    
+    //MARK: Proerties
+    var course = Course()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        nameCourseLabel.text = course.name
+        gradeLabel.text = String(course.grade!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    //MARK: Utils
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+}
---
+++ Grades/Grades/Controllers/GradesViewController.swift
@@ -0,0 +1,86 @@
+//
+//  GradesViewController.swift
+//  Grades
+//
+//  Created by S209e19 on 26/02/16.
+//  Copyright © 2016 UdeM. All rights reserved.
+//
+
+import UIKit
+
+class GradesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // MARK: - Properties
+    var courses = [Course]()
+    let cellIdentifier = "gradeCell"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        courses = Course.courses()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        if let detailVC = segue.destinationViewController as? DetailViewController{
+            if let course = sender as? Course{
+                detailVC.course = course
+            }
+        }
+    }
+
+    // MARK: - UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return courses.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+        let course = courses[indexPath.row]
+        cell?.textLabel?.numberOfLines = 0
+        cell?.textLabel?.text = course.name
+        cell?.detailTextLabel?.text = String(course.grade!)
+        
+        return cell!
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let course = courses[indexPath.row]
+        self.performSegueWithIdentifier("detail", sender: course)
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
